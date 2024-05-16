4ed5d7e8da3a5fda0dc159a3124121a326f7dba3
Georgios Rounis <georg.rounis@gmail.com>
2024-05-16 17:57:07 +0200

    fix minor issue

--- src/main/java/Gero/dev/runnerz/run/RunController.java
+++ src/main/java/Gero/dev/runnerz/run/RunController.java
@@ -1,15 +1,24 @@
 package Gero.dev.runnerz.run;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class RunController {
 
-  @GetMapping("/hello")
-  String home() {
-    return  "Hello Runner!";
+  private final RunRepository runRepository;
+
+  public RunController(RunRepository runRepository) {
+    this.runRepository = runRepository;
   }
+
+  @GetMapping("/api/runs")
+  List<Run> findAll() {
+    return runRepository.findAll();
+  }
+
+
 
 }
